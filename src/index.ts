b7556e326a3fe4ef5646108b6e1c379cb972cdde
ThaminduNR <thaminduniwantha777@gmail.com>
2024-01-01 14:31:00 +0530

    express config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log('Hello World');
-console.log('Hello Thamindu');
+import express from 'express';
+
+//invoke express
+const app = express();
+//start server
+app.listen(8080,()=>{
+    console.log("Server start 8080");
+});
