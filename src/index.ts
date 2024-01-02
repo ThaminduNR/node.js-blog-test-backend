e62fe8b63a8f14ff4a99d6a27a7e71ce056dcd89
ThaminduNR <thaminduniwantha777@gmail.com>
2024-01-02 13:06:42 +0530

    create array and push data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import bodyParser  from "body-parser";
+
 
 //invoke express
 const app = express();
+
+app.use(bodyParser.json());
+
+
+
 //start server
 app.listen(8080, () => {
     console.log("Server start 8080");
 });
 
+interface user{
+    username:string,
+    fname: string,
+    lname: string,
+    email: string,
+    password: string,
+
+}
+
+ let users:user[] = [];
+
+//get all users
 app.get('/user/all', (req: express.Request, res: express.Response) => {
 
     let data = {
@@ -18,5 +37,15 @@ app.get('/user/all', (req: express.Request, res: express.Response) => {
 
     }
 
-    res.send(data);
+    res.send(users);
 })
+
+//save user
+app.post('/user',(req:express.Request, res:express.Response)=>{
+    const request_body:any = req.body;
+    // console.log(request_body);
+
+    users.push(request_body);
+
+    res.send("Ok");
+});
