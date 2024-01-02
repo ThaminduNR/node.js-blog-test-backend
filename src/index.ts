c9e180e67e73f68007336d338ab2c1a020b5ae01
ThaminduNR <thaminduniwantha777@gmail.com>
2024-01-02 09:07:48 +0530

    pass json object

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@ import express from 'express';
 //invoke express
 const app = express();
 //start server
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Server start 8080");
 });
+
+app.get('/user/all', (req: express.Request, res: express.Response) => {
+
+    let data = {
+        id: "01234",
+        username:"thami",
+        fName: "thamindu",
+        lName: "ranawaka",
+        email: "thamindu@gmail.com"
+
+    }
+
+    res.send(data);
+})
